7ae796dffebc7a33cebae89c82261a64b55825d0
Filip Stankovic <f.stankovic@levi9.com>
2021-11-23 10:12:13 +0100

    package lock restored

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello there, General Kenobi </p> 
-        <p> Hello there, General Kenobi</p>
+        <p> Hello there, General Kenobi !!!</p>
         <p>Hello there, General Kenobi</p>
         <a
           className="App-link"
